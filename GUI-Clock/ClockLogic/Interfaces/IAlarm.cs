d8c334791a37f49135714db504a72f040840f767
Frida Gidlund <frida.gidlund@yaelev.se>
2017-10-18 10:20:46 +0200

    CodeLayout

---
+++ GUI-Clock/ClockLogic/Interfaces/IAlarm.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClockLogic
+{
+    interface IAlarm
+    {
+        bool CheckAlarm();
+        void SetAlarm(int hour, int minute);
+    }
+}
---
+++ GUI-Clock/ClockLogic/Interfaces/IMinute.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClockLogic
+{
+    interface IMinute
+    {
+        int minuteValue { get; set; }
+
+        void Tick();
+    }
+}
